4b5c6e67ecab6cc3de19fce80da59263cbab3330
Arnaud Salvucci <arnaud.salvucci@univ-amu.fr>
2021-03-24 23:17:31 +0100

    First commit

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+require('../css/app.scss');
+
+var $ = require('jquery')
+require('bootstrap')
+var hljs = require('highlightjs')
+
+hljs.initHighlightingOnLoad();
+
+/* test */
+alert('To be continued...');
+
+/* test jQuery */
+$('h2').css('background-color', 'red');
